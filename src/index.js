77b5ebaada8965abf29bf4d9ebcea723fee506f3
JensErhardt <jens-erhardt@web.de>
2018-07-07 12:03:47 +0200

    second commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'bulma/css/bulma.css'
+
+class App extends React.Component {
+  render() {
+
+    return (
+      <div>
+        <Button className=""/>
+        <Button className=""/>
+        <Button className=""/>
+      </div>
+    );
+  }
+}
+
+class Button extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  getClassName() {
+    let res = 'button';
+    if (this.props.className)
+    res += ' ' + this.props.className;
+    let types = {
+      isPrimary: 'is-primary',
+      isSuccess: 'is-success',
+      isDanger: 'is-danger',
+    };
+    for (const key in types) {
+      if (this.props[key]) {
+        res += ' ' + types[key];
+      }
+    }
+    return res;
+  }
+  render() {
+    return (
+      <button className={this.getClassName()}>
+        {this.props.children}
+      </button>
+    )
+  }
+}
+
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
